7527e8f192dcaf4fd8f711baae643789331c7a05
denmarklobo <91014643+denmarklobo@users.noreply.github.com>
2024-05-28 01:27:12 +0800

    Add files via upload

---
+++ routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\PrescriptionController;
+use App\Http\Controllers\GlassController;
+use App\Http\Controllers\HistoryController;
+use App\Http\Controllers\ProductController;
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
+Route::put('/products/{product}', [ProductController::class, 'update']);
+Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+
+Route::get('/history', [HistoryController::class, 'index']);
+Route::post('/history', [HistoryController::class, 'store']);
+Route::get('/history/{history}', [HistoryController::class, 'show']);
+Route::put('/history/{history}', [HistoryController::class, 'update']);
+Route::delete('/history/{history}', [HistoryController::class, 'destroy']);
+
+Route::get('/glass', [GlassController::class, 'index']);
+Route::post('/glass', [GlassController::class, 'store']);
+Route::get('/glass/{glass}', [GlassController::class, 'show']);
+Route::put('/glass/{glass}', [GlassController::class, 'update']);
+Route::delete('/glass/{glass}', [GlassController::class, 'destroy']);
+
+Route::get('/prescriptions', [PrescriptionController::class, 'index']);
+Route::post('/prescriptions', [PrescriptionController::class, 'store']);
+Route::get('/prescriptions/{prescription}', [PrescriptionController::class, 'show']);
+Route::put('/prescriptions/{prescription}', [PrescriptionController::class, 'update']);
+Route::delete('/prescriptions/{prescription}', [PrescriptionController::class, 'destroy']);
+
+Route::get('/patients', [PatientController::class, 'index']);
+Route::post('/patients', [PatientController::class, 'store']);
+Route::get('/patients/{patient}', [PatientController::class, 'show']);
+Route::put('/patients/{patient}', [PatientController::class, 'update']);
+Route::delete('/patients/{patient}', [PatientController::class, 'destroy']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\PrescriptionController;
+use App\Http\Controllers\GlassController;
+use App\Http\Controllers\HistoryController;
+use App\Http\Controllers\ProductController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+
+
+Route::get('/history', [HistoryController::class, 'index'])->name('history.index');
+Route::post('/history', [HistoryController::class, 'store'])->name('history.store');
+Route::get('/history/{history}', [HistoryController::class, 'show'])->name('history.show');
+Route::put('/history/{history}', [HistoryController::class, 'update'])->name('history.update');
+Route::delete('/history/{history}', [HistoryController::class, 'destroy'])->name('history.destroy');
+
+Route::get('/glass', [GlassController::class, 'index'])->name('glass.index');
+Route::post('/glass', [GlassController::class, 'store'])->name('glass.store');
+Route::get('/glass/{glass}', [GlassController::class, 'show'])->name('glass.show');
+Route::put('/glass/{glass}', [GlassController::class, 'update'])->name('glass.update');
+Route::delete('/glass/{glass}', [GlassController::class, 'destroy'])->name('glass.destroy');
+
+Route::get('/prescriptions', [PrescriptionController::class, 'index'])->name('prescriptions.index');
+Route::post('/prescriptions', [PrescriptionController::class, 'store'])->name('prescriptions.store');
+Route::get('/prescriptions/{prescription}', [PrescriptionController::class, 'show'])->name('prescriptions.show');
+Route::put('/prescriptions/{prescription}', [PrescriptionController::class, 'update'])->name('prescriptions.update');
+Route::delete('/prescriptions/{prescription}', [PrescriptionController::class, 'destroy'])->name('prescriptions.destroy');
+
+Route::get('/patients', [PatientController::class, 'index'])->name('patients.index');
+Route::post('/patients', [PatientController::class, 'store'])->name('patients.store');
+Route::get('/patients/{patient}', [PatientController::class, 'show'])->name('patients.show');
+Route::put('/patients/{patient}', [PatientController::class, 'update'])->name('patients.update');
+Route::delete('/patients/{patient}', [PatientController::class, 'destroy'])->name('patients.destroy');
+
+Route::get('/', function () {
+    return view('welcome');
+});
